04ac8adf26b25e492b0b7dec2dd60f43b25d8aca
plancsar <plancsar@users.noreply.github.com>
2022-04-08 16:57:40 +0200

    sha256 checksum

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -32,7 +32,7 @@ $packageArgs = @{
   # To determine checksums, you can get that from the original site if provided.
   # You can also use checksum.exe (choco install checksum) and use it
   # e.g. checksum -t sha256 -f path\to\file
-  checksum      = ''
+  checksum      = '2BB2F6C504D81BEA58ED34ABA26669F88489E1E52D785976CA119310F0848592'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
   checksum64    = ''
   checksumType64= 'sha256' #default is checksumType
